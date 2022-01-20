91554fd827919354b7093298d5c24ca7bf362457
serraguti <46522403+serraguti@users.noreply.github.com>
2022-01-20 18:47:20 +0100

    Add project files.

---
+++ MvcApiCrudDepartamentos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcApiCrudDepartamentos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MvcApiCrudDepartamentos/Services/ServiceDepartamentos.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using MvcApiCrudDepartamentos.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MvcApiCrudDepartamentos.Services
+{
+    public class ServiceDepartamentos
+    {
+        private string URL;
+        private MediaTypeWithQualityHeaderValue Header;
+
+        public ServiceDepartamentos(string url)
+        {
+            this.URL = url;
+            this.Header =
+                new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        //LOS METODOS GET SI UTILIZAN GENERICOS
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.URL);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response =
+                    await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+            string request = "/api/departamentos";
+            List<Departamento> departamentos =
+                await this.CallApiAsync<List<Departamento>>(request);
+            return departamentos;
+        }
+
+        public async Task<Departamento> FindDepartamentoAsync(int id)
+        {
+            string request = "/api/departamentos/" + id;
+            Departamento dept = await this.CallApiAsync<Departamento>(request);
+            return dept;
+        }
+
+        public async Task DeleteDepartamentoAsync(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/departamentos/" + id;
+                client.BaseAddress = new Uri(this.URL);
+                client.DefaultRequestHeaders.Clear();
+                await client.DeleteAsync(request);
+            }
+        }
+
+        public async Task InsertDepartamentoAsync(int id, string nombre, string localidad)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/departamentos";
+                client.BaseAddress = new Uri(this.URL);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                Departamento dept = new Departamento();
+                dept.IdDepartamento = id;
+                dept.Nombre = nombre;
+                dept.Localidad = localidad;
+                //DEBEMOS CONVERTIR A JSON EL OBJETO
+                String json =
+                    JsonConvert.SerializeObject(dept);
+                StringContent content =
+                    new StringContent(json, Encoding.UTF8, "application/json");
+                await client.PostAsync(request, content);
+            }
+        }
+
+
+    }
+}
